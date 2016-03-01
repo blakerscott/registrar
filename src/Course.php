5f0c3c666b7921cd3cd3e1f398527fbd5e234979
Blake Scott and Jon Nardine <blake+jon@>
2016-03-01 14:38:23 -0800

    add logic to pass get and add student tests for the course class

--- src/Course.php
+++ src/Course.php
@@ -76,5 +76,30 @@
 				return $found_course;
 		}
 
+		function addStudent($student)
+    {
+        $GLOBALS['DB']->exec("INSERT INTO students_courses (course_id, student_id) VALUES ({$this->getId()}, {$student->getId()});");
+    }
+
+    function getStudents()
+    {
+      $query = $GLOBALS['DB']->query("SELECT student_id FROM students_courses WHERE course_id = {$this->getId()};");
+      $student_ids = $query->fetchAll(PDO::FETCH_ASSOC);
+
+      $students = array();
+      foreach($student_ids as $id) {
+          $student_id = $id['student_id'];
+          $result = $GLOBALS['DB']->query("SELECT * FROM students WHERE id = {$student_id};");
+          $returned_student = $result->fetchAll(PDO::FETCH_ASSOC);
+
+          $name = $returned_student[0]['name'];
+          $id = $returned_student[0]['id'];
+          $add_date = $returned_student[0]['add_date'];
+          $new_student = new Student($id, $name, $add_date);
+          array_push($students, $new_student);
+      }
+      return $students;
+    }
+
 	}
  ?>
